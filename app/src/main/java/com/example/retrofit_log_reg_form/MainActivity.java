65a75bddd5149206cdab3cd561ac1a412212f0ea
devfast01 <iot123tech@gmail.com>
2023-05-12 10:41:17 +0500

    hyu

---
+++ app/src/main/java/com/example/retrofit_log_reg_form/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.retrofit_log_reg_form;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
